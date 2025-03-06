79154936b5bc9337cfcac18de34c039da5659123
sahil <sahilramesh.mane2022@vitstudent.ac.in>
2025-03-06 21:29:24 +0530

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,46 @@
+//const name ="sahil";
+const repocount = 50;
+//console.log("My name is "+name+" and I have "+repocount+" repositories");
+
+//console.log(`hello my name is ${name} and have ${repocount}`);
+
+//decleare sring
+const gname = new String("sahiL");
+console.log(gname[0]);
+console.log(gname.__proto__);
+console.log(gname.length);
+//some onjects
+console.log(gname.charAt(2));
+console.log(gname.toLocaleUpperCase());
+console.log(gname.toLowerCase());
+console.log(gname.indexOf("h"));
+console.log(gname.lastIndexOf("h"));
+console.log(gname.startsWith("s"));
+console.log(gname.endsWith("L"));
+console.log(gname.includes("a"));
+console.log(gname.substring(1,3));
+console.log(gname.slice(1,3));
+console.log(gname.split(""));
+console.log(gname.trim());
+console.log(gname.trimLeft());
+console.log(gname.trimRight());
+console.log(gname.repeat(2));
+console.log(gname.replace("L","l"));
+//console.log(gname.concat(" is good");
+//0:
+// "s"
+// 1
+// : 
+// "a"
+// 2
+// : 
+// "h"
+// 3
+// : 
+// "i"
+// 4
+// : 
+// "l"
+// length
+// : 
+// 5
